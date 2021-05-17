39619d3ac602c7ecfa01b79bdfc1ddfc6d64de23
Cerelek <skuzajan@gmail.com>
2021-05-17 12:41:07 +0200

    wywalenie new prefix
    Nikt się tym nie zajmuje więc wywalam

---
+++ useful trash/newprefix.js
@@ -0,0 +1,10 @@
+const index = require('../src/index');
+module.exports = {
+    name: 'newprefix',
+    description: 'set new prefix',
+    execute(msg, args){
+        //index.prefix = 
+    }
+
+
+}

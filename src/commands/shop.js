7f4f65bca6d3e54ee0f45a5d00503e6561068033
Cerelek <skuzajan@gmail.com>
2021-05-24 23:44:28 +0200

    Update shop.js

--- src/commands/shop.js
+++ src/commands/shop.js
@@ -3,12 +3,24 @@ const Discord = require('discord.js');
 module.exports = {
     name: 'shop',
     description: 'show shop',
-    execute(msg, args){
+    async execute(msg, args){
+
+        const one ="1️⃣";
+        const two ="2️⃣";
+        const three ="3️⃣";
+
+
         const shopEmbed = new Discord
         .RichEmbed()
         .setColor(0xe0b336)
         .setTitle(`======SHOP======`)
-        .setDescription(`sword - 46 G`)
-        msg.channel.send(shopEmbed);
+        .setDescription(`1️⃣ Blue Rose Sword - 250 G \n
+2️⃣ Stick - 30 G \n
+3️⃣ Plain Book - 80 G`)
+        let mesg = await msg.channel.send(shopEmbed);
+        await mesg.react(one)
+        await mesg.react(two)
+        await mesg.react(three)
+        const reactions = await mesg.awaitReactions((reaction, user) => user.id === msg.author.id && (reaction.emoji.name === one || reaction.emoji.name === two || reaction.emoji.name === three), {time: 5000})
     },
 }
